1d586d7002d62d10aaabac76d6285782d47b6fd6
이준혁 <leejunhyeok@z2h427.local>
2024-11-15 19:59:03 +0900

    1115

--- components/list-post.tsx
+++ components/list-post.tsx
@@ -35,7 +35,7 @@ export default function ListProduct({
   return (
     <Link href={`/products/${id}`} className="flex gap-5">
       <div className="relative size-20 rounded-md overflow-hidden">
-        <img src={photo} alt={title} className="object-cover" />
+        <Image src={photo} alt={title} className="object-cover" />
       </div>
       <div className="flex flex-col gap-1 *:text-white">
         <span className="text-lg">{title}</span>
